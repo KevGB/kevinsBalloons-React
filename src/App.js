980e810337258cb9bb5f54711a4239593464c46c
KevGB <kevingbice@gmail.com>
2023-02-14 14:12:47 +0200

    App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import Logo from './assets/img/KBlogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar sticky='top' expand='md' color='primary' dark>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={Logo} alt="Kevin's Balloons Logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
     </div>
   );
 }
