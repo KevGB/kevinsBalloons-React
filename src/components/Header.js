e462acc253d2f1f458d336a6a713b1e623b5ca28
KevGB <kevingbice@gmail.com>
2023-02-17 09:45:04 +0200

    Creates Navigation and Header Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,20 @@
-import {
-  Navbar,
-  NavbarBrand,
-  Collapse,
-  NavbarToggler,
-  Nav,
-  NavItem,
-} from "reactstrap";
-import { NavLink } from "react-router-dom";
-import Logo from "../assets/img/KBlogo.png";
-import { useState } from "react";
+import { Container } from "reactstrap";
 
-const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const Header = (props) => {
+  const {image, headline, tagline, display} = props;
 
   return (
-    <Navbar sticky="top" expand="md" color="primary" dark>
-      <NavbarBrand href="/">
-        <img src={Logo} alt="Kevin's Balloons Logo" />
-        <h1>Kevin's Balloons</h1>
-      </NavbarBrand>
+    <Container className="container-fluid header" style={{backgroundImage: `URL(${image})`}}>
+     <div className="overlay">
+      <h1>{headline}</h1>
+      <p>{tagline}</p>
+      <div className="header-btn-container" style={{display: {display}}}>
+        <button className="btn header-btn header-subscribe bg-white">Subscribe</button>
+        <button className="btn header-btn header-book bg-primary">Book Kevin</button>
+      </div>
+     </div>
 
-      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-
-      <Collapse isOpen={menuOpen} navbar>
-        <Nav className="ms-auto" navbar>
-          <NavItem>
-            <NavLink className="nav-link" to="/book">
-              Book
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="nav-link" to="/services">
-              Services
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="nav-link" to="/contact">
-              Contact
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="nav-link" to="/gallery">
-              Gallery
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    </Container>
   );
 };
 
