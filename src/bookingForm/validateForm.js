9ec8288b1b45ed2d97c15a4103b6e4a1047fac04
KevGB <kevingbice@gmail.com>
2023-02-23 16:07:55 +0200

    Creates regex for email and phone

--- src/bookingForm/validateForm.js
+++ src/bookingForm/validateForm.js
@@ -1,1 +1,20 @@
-const phoneReg = /\(?(?<areacode>\d{3}\)?[ -]?(\d{3})[ -]?(\d{4})/
+export const validateForm = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!validEmail.test(values.email)) {
+    errors.email = "Check that this is a valid email address";
+  }
+
+  if(!values.phone){
+    errors.phone = "Required";
+  }else if (!validPhone.test(values.phone)){
+    errors.phone = "Only use numbers and spaces (and a + if you're including a country code)"
+  }
+  
+
+};
+
+const validPhone = /^(?:(\+\d+)[ -])?\(?\d{3}\)?[ -]?(\d{3})[ -]?(\d{4})$/g;
+const validEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z.]+$/i;
