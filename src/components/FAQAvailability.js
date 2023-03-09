81bad0254b7fcc26d1f816d5b90d64f09bb1f225
KevGB <kevingbice@gmail.com>
2023-03-09 11:47:10 -0800

    Creates FAQAvailability.js

---
+++ src/components/FAQAvailability.js
@@ -0,0 +1,11 @@
+export const FAQAvailability = () => {
+  return (
+    <p>
+      Check Kevins availability below, but even if he isnt available, we still
+      encourage you to continue booking your event. If there is a cancellation
+      we will immediately let you know we can come to your event. Week days are
+      usually wide open, so consider an event at your school, daycare, or summer
+      camp!
+    </p>
+  );
+};
--- src/pages/WhenForm.js
+++ src/pages/WhenForm.js
@@ -1,25 +1,42 @@
-import { Formik, Field, Form, FormGroup } from "formik";
-import { Label } from "reactstrap";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Label, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FAQAvailability } from "../components/FAQAvailability";
 
 const WhenForm = () => {
+  const [faqShow, toggleFaqShow] = useState(false);
+
+  const handleClick = () => {
+    toggleFaqShow(!faqShow);
+  };
+
   return (
     <div className="form">
       <h1>Tell us about your event</h1>
-    <Formik>
-      <Form>
-        <FormGroup>
-          <Label>When is your event?</Label>
-          <Field></Field>
-        </FormGroup>
-      </Form>
-    </Formik>
-      
+      <Formik>
+        <Form>
+          <FormGroup className="form">
+            <Label>When is your event?</Label>
+            <Field name="date" type="date" className="form-control" />
+            <ErrorMessage name="date">
+              {(msg) => <p className="text-danger ">{msg}</p>}
+            </ErrorMessage>
+            <Field name="time" type="time" className="form-control" />
+            <ErrorMessage name="time">
+              {(msg) => <p className="text-danger ">{msg}</p>}
+            </ErrorMessage>
+          </FormGroup>
+        </Form>
+        <p><a href="#0" onClick={handleClick}>FAQ: What is Kevin's Availability?</a></p>
+        {faqShow && <FAQAvailability/>}
       <Link className="nav-link" to="/where">
         <button className="btn btn-primary" type="submit">
           Next{" "}
         </button>
       </Link>
+      </Formik>
+
       <div className="form-navigation">
         <Link to="/">
           <i className="fa fa-solid fa-circle " />
