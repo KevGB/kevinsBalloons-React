19a74f4a0f11cbc35879ca73d4377d10678b213a
KevGB <kevingbice@gmail.com>
2023-03-15 17:10:02 -0800

    Updates T&C

--- src/utils/TandC.js
+++ src/utils/TandC.js
@@ -4,8 +4,7 @@ export const TandC = () => {
       <h3>Kevin's Balloons Terms and Conditions</h3>
       <p>As of March 9, 2023</p>
       <p>
-        By hiring Kevin Bice you agree to the following terms and conditions:
-      </p>
+      These terms and conditions ("Agreement") constitute a legally binding agreement between the Balloon Artist ("Artist") and the client ("Client"). By booking the Artist's services, the Client agrees to be bound by this Agreement.      </p>
       <ol>
         <li>
           I agree to be contected by Kevin's Balloons about my inquiry, and be
@@ -13,11 +12,14 @@ export const TandC = () => {
           for discounts, so we won't spam up your inbox
         </li>
         <li>All sales are final. There are no refunds.</li>
+        <li>If you need to cancel an event, that's ok. Just notify us in writing as soon as possible. Deposits will not be refunded, but the remaining balance does not need to be paid. Either party may terminate this Agreement at any time upon written notice to the other party.</li>
+        <li>After the initial deposit is payed online, the remaining balance, agreed upon over email, will be due in cash at the end of the event.</li>
         <li>There are no refunds for popped balloons, but if time allows you can often get a balloon repaired or replaced</li>
         <li>
           Delivery windows are estimates and we cannot guarantee delivery times.
         </li>
         <li>No event is booked until payment has been received.</li>
+        <li>NThe Artist will not be liable for any damages or losses arising from the services provided. The Client is responsible for any damages caused to the Artist's equipment or materials during the Event.</li>
         <li>
           No bouquet, arrangement, balloon installation, or any other item is
           considered ordered until payment has been received.
@@ -30,6 +32,7 @@ export const TandC = () => {
         <li>
           You agree to indemnify and hold harmless Kevin's Balloons from any claims surround COVID-19.
         </li>
+        <li>By booking the Artist's services, the Client acknowledges that they have read and understood this Agreement and agree to be bound by its terms and conditions.</li>
       </ol>
     </div>
   );
